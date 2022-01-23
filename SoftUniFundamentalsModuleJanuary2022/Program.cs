169990341347550bdf6a596cc718baca4632aab8
pavel <pavel_ts_pavlov@outlook.com>
2022-01-23 15:06:47 +0200

    Task-1-restructure

--- SoftUniFundamentalsModuleJanuary2022/Program.cs
+++ SoftUniFundamentalsModuleJanuary2022/Program.cs
@@ -1,30 +1,35 @@
 ﻿using System;
 
-namespace SoftUniFundamentalsModuleJanuary2022
+namespace Task2Division
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
-
-            int age = int.Parse(Console.ReadLine());
-            if (age >= 0 && age <= 2)
+            int number = int.Parse(Console.ReadLine());
+            if (number % 10 == 0)
             {
-                Console.WriteLine("baby");
-            }else if (age >= 3 && age <= 13)
-            {
-                Console.WriteLine("child");
-            }else if (age >= 14 && age <= 19)
-            {
-                Console.WriteLine("teenager");
+                Console.WriteLine($"The number is divisible by 10");
             }
-            else if (age >= 20 && age <= 65)
+            else if (number % 7 == 0)
             {
-                Console.WriteLine("adult");
+                Console.WriteLine($"The number is divisible by 7");
             }
-            else if (age >= 66)
+            else if (number % 6 == 0)
             {
-                Console.WriteLine("elder");
+                Console.WriteLine($"The number is divisible by 6");
+            }
+            else if (number % 3 == 0)
+            {
+                Console.WriteLine($"The number is divisible by 3");
+            }
+            else if (number % 2 == 0)
+            {
+                Console.WriteLine($"The number is divisible by 2");
+            }
+            else
+            {
+                Console.WriteLine($"Not divisible");
             }
         }
     }
