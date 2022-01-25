aacfee7877f225ed5dedd19b688dbadff4904854
pavel <pavel_ts_pavlov@outlook.com>
2022-01-25 22:53:36 +0200

    Task-4-Print-and-sum

---
+++ Task4PrintAndSum/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Task4PrintAndSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int SecondNumber = int.Parse(Console.ReadLine());
+            int sum=0;
+
+            do
+            {
+
+                Console.Write($"{firstNumber} ");
+                
+                sum += firstNumber;
+
+                firstNumber += 1;
+            } while (firstNumber <= SecondNumber) ;
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
