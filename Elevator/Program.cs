f36b0f46ff645fd7be335e6db5ef081986b34d8b
pavel <pavel_ts_pavlov@outlook.com>
2022-02-13 16:48:38 +0200

    Task Elevator completed

---
+++ Elevator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Elevator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int people = int.Parse(Console.ReadLine());
+            int capacity = int.Parse(Console.ReadLine());
+            int trips;
+            int remainingPassangers = people % capacity;
+
+            if (people > capacity)
+            {
+                if (remainingPassangers != 0)
+                {
+                    trips = people / capacity+ 1;
+                }
+                else
+                {
+                    trips = people / capacity;
+                }
+            }
+            else
+            {
+                
+                trips = 1;
+            }
+            
+            Console.WriteLine(trips);
+        }
+    }
+}
