4d812f869ad246524da2771f11e5f0874983a424
pavel <pavel_ts_pavlov@outlook.com>
2022-02-13 21:23:17 +0200

    TriplesOfLatinLetters WiP

--- SumDigits/Program.cs
+++ SumDigits/Program.cs
@@ -7,22 +7,12 @@ namespace SumDigits
         static void Main(string[] args)
         {
             string number = Console.ReadLine();
-          //  string[] numbersArray = new string[number.Length];
             int result = 0;
-           // char[] charsArray = new char[numbersArray.Length];
-
             char[] charArr = number.ToCharArray();  
-            foreach (char ch in charArr)  
-            {  
-                Console.WriteLine(ch);  
-            }   
 
             for (int i = 0; i < charArr.Length; i++)
             {
-                Console.WriteLine(charArr[i]);
-                
-                result +=charArr[i];
-                
+                result += int.Parse(charArr[i].ToString());
             }
             Console.WriteLine(result);
         }
---
+++ TriplesОfLatinLetters/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace TriplesОfLatinLetters
+{
+    using System;
+        
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            char firstLetter = 'a';
+           // a=97
+
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write(firstLetter);
+                
+                for (int j = 0; j < number; j++)
+                {
+                    Console.Write(firstLetter);
+                    
+                    for (int k = 0; k < number; k++)
+                    {
+                        Console.WriteLine(firstLetter);
+                        k++;
+                    }
+                    j++;
+                }
+                i++;
+            }
+        }
+    }
+}
