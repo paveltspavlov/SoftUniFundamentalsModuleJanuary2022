75af46f91944f257429f234c9a25c5aa1b181141
pavel <pavel_ts_pavlov@outlook.com>
2022-02-13 19:04:31 +0200

    Print Part of the ASCII Table

---
+++ PrintAsciiTable/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace PrintAsciiTable
+{
+    using System;
+        
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int finish = int.Parse(Console.ReadLine());
+            char output;
+            do
+            {
+                output = Convert.ToChar(start);
+                Console.Write($"{output} ");
+                start++;
+            } while (start <= finish);
+        }
+    }
+}
