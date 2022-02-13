aa1a5264c0b51f5aba0df741a5aa03482551c6e3
pavel <pavel_ts_pavlov@outlook.com>
2022-02-13 18:32:03 +0200

    Sum of chars done

---
+++ SumOfChars/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace SumOfChars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int sum=0;
+            for (int i = 0; i < num; i++)
+            {
+                char currentChar = char.Parse(Console.ReadLine());
+                sum += currentChar;
+            }
+            Console.WriteLine($"The sum equals: {sum}");
+        }
+    }
+}
